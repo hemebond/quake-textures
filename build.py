bdbfdb1ed750463df989f2201f0088bf83f9af12
James O'Neill <hemebond@gmail.com>
2021-11-28 17:35:50 +0900

    Minor fixes and reformatting

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from PIL import Image, ImageEnhance
-import glob, os
+import glob
+import os
+from PIL import Image
 import shutil
 import yaml
 import argparse
@@ -30,8 +31,7 @@ DEBUG = False
 def scale_pixel(pixel, minimum, maximum):
 	delta = maximum - minimum
 
-	if DEBUG:
-		print(pixel, minimum, maximum, delta, sep=', ')
+	log.debug(', '.join([pixel, minimum, maximum, delta]))
 
 	def scale(pixel_value):
 		return int(((pixel_value / 255) * delta + minimum) * 255)
@@ -49,70 +49,69 @@ def scale_pixel(pixel, minimum, maximum):
 
 
 def scale_layer(im, minimum, maximum):
+	log.debug(' '.join([im, minimum, maximum]))
+
 	pixels = im.load()
-
-	if DEBUG:
-		print(im, minimum, maximum)
-
 	for i in range(im.size[0]):
 		for j in range(im.size[1]):
 			px = pixels[i, j]
-			pixels[i,j] = scale_pixel(px, minimum, maximum)
+			pixels[i, j] = scale_pixel(px, minimum, maximum)
 
 
-parser = argparse.ArgumentParser(description='Build bump map images for Quake textures.')
-parser.add_argument('-i', '--input',  default="textures", type=str, help='Directory containing the variant textures')
-parser.add_argument('-t', '--type', default='all', type=str, help="Comma-separated list of texture variants to build. Defaults to `all` which builds all variants available.")
-parser.add_argument('-o', '--output', default="~/.darkplaces/id1/textures", type=str, help='Quake textures directory to put the bump map images.')
-parser.add_argument('-f', '--format', default='tga', type=str, help='Image format to use. (TODO)')
-parser.add_argument('-l', '--log-level', default="info", help="Log level")
-args = parser.parse_args()
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Build bump map images for Quake textures.')
+	parser.add_argument('-i', '--input', default="textures", type=str, help='Directory containing the variant textures')
+	parser.add_argument('-t', '--type', default='all', type=str, help="Comma-separated list of texture variants to build. Defaults to `all` which builds all variants available.")
+	parser.add_argument('-o', '--output', default="~/.darkplaces/id1/textures", type=str, help='Quake textures directory to put the bump map images.')
+	parser.add_argument('-f', '--format', default='tga', type=str, help='Image format to use. (TODO)')
+	parser.add_argument('-l', '--log-level', default="info", help="Log level")
+	args = parser.parse_args()
 
 
-log.setLevel(logging._nameToLevel[args.log_level.upper()])
+	log.setLevel(logging._nameToLevel[args.log_level.upper()])
 
 
-if args.type == 'all':
-	# args.type = TEXTURE_VARIANTS
-	# temp override
-	args.type = ('bump', 'gloss', 'glow', "norm")
-else:
-	args.type = args.type.split(',')
+	if args.type == 'all':
+		# args.type = TEXTURE_VARIANTS
+		# temp override
+		args.type = ('bump', 'gloss', 'glow', "norm")
+	else:
+		args.type = args.type.split(',')
 
-t_defs = {}
-for variant in args.type:
-	try:
-		with open(os.path.abspath(os.path.expanduser(variant + '.yml')), 'r') as fp:
-			t_defs[variant] = yaml.safe_load(fp)
-	except FileNotFoundError:
-		t_defs[variant] = {}
+	t_defs = {}
+	for variant in args.type:
+		try:
+			with open(os.path.abspath(os.path.expanduser(variant + '.yml')), 'r') as fp:
+				t_defs[variant] = yaml.safe_load(fp)
+		except FileNotFoundError:
+			t_defs[variant] = {}
 
-# Set abolute paths for input and output directories
-args.input  = os.path.abspath(os.path.expanduser(args.input))
-args.output = os.path.abspath(os.path.expanduser(args.output))
+	# Set abolute paths for input and output directories
+	args.input = os.path.abspath(os.path.expanduser(args.input))
+	args.output = os.path.abspath(os.path.expanduser(args.output))
 
-# Check that the output directory exists
-if not os.path.isdir(args.output):
-	raise NotADirectoryError(f"The output directory `{args.output}` does not exist")
+	# Check that the output directory exists
+	if not os.path.isdir(args.output):
+		raise NotADirectoryError(f"The output directory `{args.output}` does not exist")
 
 
-diffuse_textures = {}
-for tex_path in glob.glob(os.path.join(args.input, "diffuse", "*")):
-	tex_filename = os.path.basename(tex_path) # window01_4.tga
-	tex_name, tex_ext = os.path.splitext(tex_filename) # (window01_4, .tga)
-	diffuse_textures[tex_name] = [tex_name]
+	diffuse_textures = {}
+	for tex_path in glob.glob(os.path.join(args.input, "diffuse", "*")):
+		tex_filename = os.path.basename(tex_path)  # window01_4.tga
+		tex_name, tex_ext = os.path.splitext(tex_filename)  # (window01_4, .tga)
+		diffuse_textures[tex_name] = [tex_name]
 
-with open(os.path.abspath(os.path.expanduser('diffuse.yml')), "r") as fp:
-	t_defs['diffuse'] = yaml.safe_load(fp)
-diffuse_textures.update(t_defs['diffuse'])
+	with open(os.path.abspath(os.path.expanduser('diffuse.yml')), "r") as fp:
+		t_defs['diffuse'] = yaml.safe_load(fp)
+	diffuse_textures.update(t_defs['diffuse'])
 
-if True:
 	for t_name, t_src in diffuse_textures.items():
 		# copy over the diffuse textures
 		t_ext = '.tga'
 		t_filename = t_name + t_ext
 		t_path = os.path.abspath(os.path.join(args.input, 'diffuse', t_src[0] + t_ext))
-		t_format = t_ext[1:] # tga
+		t_format = t_ext[1:]  # tga
 
 		src = t_path
 		dst = os.path.abspath(os.path.join(args.output, t_filename))
@@ -132,7 +131,7 @@ if True:
 
 			dst = os.path.abspath(os.path.join(args.output, dst_filename))
 
-			if variant in t_defs and  t_name in t_defs[variant]:
+			if variant in t_defs and t_name in t_defs[variant]:
 				# If variant definition use that
 				layers = t_defs[variant][t_name]
 
@@ -140,7 +139,14 @@ if True:
 				# anything prefixed with __ is a special layer that isn't scaled
 				num_layers = 1
 				if len(layers) > 1:
-					num_layers = len([l for l in layers if l == None or l[:2] != '__'])
+					num_layers = len(
+						[
+							layer
+							for layer
+							in layers
+							if layer is None or layer[:2] != '__'
+						]
+					)
 
 				for layer_level, layer_name in enumerate(layers):
 					if layer_name:
@@ -156,7 +162,7 @@ if True:
 							layer = layer.convert('RGBA')
 
 						if im is None:
-							im = Image.new( 'RGBA', (layer.size[0], layer.size[1]), "black")
+							im = Image.new('RGBA', (layer.size[0], layer.size[1]), "black")
 
 						if layer_name[:2] != '__':
 							layer_min = float(layer_level) / num_layers
